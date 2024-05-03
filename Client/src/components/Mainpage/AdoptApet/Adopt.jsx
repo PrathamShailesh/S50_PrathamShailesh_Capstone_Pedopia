1e9951b7ece4668dda27af6dbabef64f4dbba0ab
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2024-05-03 14:26:51 +0530

    added communication between two users

--- Client/src/components/Mainpage/AdoptApet/Adopt.jsx
+++ Client/src/components/Mainpage/AdoptApet/Adopt.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { useNavigate } from "react-router-dom";
 
 const PetDetailsPopup = ({ pet, onClose }) => {
@@ -10,6 +12,7 @@ const PetDetailsPopup = ({ pet, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 text-center ">
       <div className="bg-white p-8 rounded-lg shadow-md w-[800px] flex ">
         <button className="absolute top-4 right-4 text-white text-3xl" onClick={onClose}>X</button>
+
         <div className="w-1/2 pr-4 flex flex-col justify-between">
           <h2 className="text-2xl font-semibold mb-4">{pet.name}</h2>
           <p className="text-gray-700 mb-4">{pet.description}</p>
